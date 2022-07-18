26ff02c96d5d27672cd07bc968463a4beed50b33
Ezan777 <ezangri@gmail.com>
2022-07-18 17:51:40 +0200

    Started the creation of a view that will show summoner information.

---
+++ lib/summoner_info_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SummonerInfo extends StatefulWidget {
+
+  const SummonerInfo({Key? key}) : super(key: key);
+
+  @override
+  State<SummonerInfo> createState() => _SummonerInfoState();
+}
+
+class _SummonerInfoState extends State<SummonerInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
