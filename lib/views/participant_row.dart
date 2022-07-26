cc20b415d72970b516eafa7f1c3f2ef713633a13
Ezan777 <ezangri@gmail.com>
2022-07-26 09:22:47 +0200

    Transformed ParticipantRow in a stateful widget

--- lib/views/participant_row.dart
+++ lib/views/participant_row.dart
@@ -1,18 +1,23 @@
 import 'package:darthus/darthus.dart';
 import 'package:flutter/material.dart';
 
-class ParticipantRow extends StatelessWidget {
+class ParticipantRow extends StatefulWidget {
   final FinishedParticipant participant;
   final BoxConstraints constraints;
 
   const ParticipantRow(
-      {Key? key, required this.constraints, required this.participant})
+      {Key? key, required this.participant, required this.constraints})
       : super(key: key);
 
   @override
+  State<ParticipantRow> createState() => _ParticipantRowState();
+}
+
+class _ParticipantRowState extends State<ParticipantRow> {
+  @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints(maxWidth: constraints.maxWidth),
+      constraints: BoxConstraints(maxWidth: widget.constraints.maxWidth),
       child: FittedBox(
         fit: BoxFit.fitWidth,
         child: Row(
@@ -21,13 +26,13 @@ class ParticipantRow extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(5),
               child: SizedBox(
-                width: 0.24 * constraints.maxWidth,
-                height: 0.25 * constraints.maxWidth,
+                width: 0.24 * widget.constraints.maxWidth,
+                height: 0.25 * widget.constraints.maxWidth,
                 child: Stack(
                   children: [
                     Container(
-                      width: 0.23 * constraints.maxWidth,
-                      height: 0.23 * constraints.maxWidth,
+                      width: 0.23 * widget.constraints.maxWidth,
+                      height: 0.23 * widget.constraints.maxWidth,
                       decoration: BoxDecoration(
                         border: Border.all(
                           color: Colors.grey.shade900,
@@ -37,15 +42,15 @@ class ParticipantRow extends StatelessWidget {
                       ),
                       child: ClipOval(
                         child: Image.asset(
-                            "assets/images/champions_tiles/${participant.championInfo["championName"]}_0.jpg"),
+                            "assets/images/champions_tiles/${widget.participant.championInfo["championName"]}_0.jpg"),
                       ),
                     ),
                     Positioned(
-                      top: 0.19 * constraints.maxWidth,
-                      left: 0.07 * constraints.maxWidth,
+                      top: 0.19 * widget.constraints.maxWidth,
+                      left: 0.07 * widget.constraints.maxWidth,
                       child: Container(
-                        width: 0.1 * constraints.maxWidth,
-                        height: 0.06 * constraints.maxWidth,
+                        width: 0.1 * widget.constraints.maxWidth,
+                        height: 0.06 * widget.constraints.maxWidth,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
                           color: Colors.grey.shade900,
@@ -55,7 +60,7 @@ class ParticipantRow extends StatelessWidget {
                         ),
                         child: Center(
                           child: Text(
-                            participant.championInfo["championLevel"]
+                            widget.participant.championInfo["championLevel"]
                                 .toString(),
                             style: Theme.of(context)
                                 .textTheme
@@ -70,7 +75,7 @@ class ParticipantRow extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               child: Column(
                 children: [
                   _itemsFirstRow(),
@@ -82,14 +87,21 @@ class ParticipantRow extends StatelessWidget {
               TextSpan(
                 style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                       fontSize: 18,
+                      color: Colors.yellow.shade800,
                     ),
-                text: "${(participant.goldEarned / 1000).toStringAsFixed(1)} k",
+                text:
+                    "${(widget.participant.goldEarned / 1000).toStringAsFixed(1)} k",
                 children: [
                   WidgetSpan(
-                    child: SizedBox(width: 0.01 * constraints.maxWidth,)
-                  ),
-                  const WidgetSpan(
-                    child: Icon(Icons.paid_outlined),
+                      child: SizedBox(
+                    width: 0.01 * widget.constraints.maxWidth,
+                  )),
+                  WidgetSpan(
+                    child: Icon(
+                      Icons.paid_outlined,
+                      color: Colors.yellow.shade800,
+                      size: 18,
+                    ),
                   ),
                 ],
               ),
@@ -144,17 +156,17 @@ class ParticipantRow extends StatelessWidget {
 
   Widget _boxItemAt(int index) {
     return Container(
-      width: 0.1 * constraints.maxWidth,
-      height: 0.1 * constraints.maxWidth,
+      width: 0.1 * widget.constraints.maxWidth,
+      height: 0.1 * widget.constraints.maxWidth,
       decoration: BoxDecoration(
         color: Colors.grey.shade300,
         borderRadius: BorderRadius.circular(10),
       ),
       child: ClipRRect(
           borderRadius: BorderRadius.circular(10),
-          child: participant.itemsId[index] != 0
+          child: widget.participant.itemsId[index] != 0
               ? Image.asset(
-                  "assets/images/items/${participant.itemsId[index]}.png")
+                  "assets/images/items/${widget.participant.itemsId[index]}.png")
               : const SizedBox()),
     );
   }
